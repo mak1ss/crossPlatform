d8be142088f11c11005c3f535833e5bf651194a6
Maksym Diachuk <dia4uk.maksim@gmail.com>
2024-03-19 11:58:41 +0200

    Initial commit. Generate project via ttcli with Tailwind CSS

---
+++ src/main/java/com/diachuk/lab/CrossPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.diachuk.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diachuk/lab/CrossPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diachuk.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
